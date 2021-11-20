f1f894907aa5bafd54da4728348a48e38adda5cd
Chaithra K V <chaithrakv98@gmail.com>
2021-11-20 12:26:03 +0530

    template 2

--- Pcare/care/urls.py
+++ Pcare/care/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('services/',views.services, name="services"),
+    path('contact/',views.contact, name="contact")
 ]
--- Pcare/care/views.py
+++ Pcare/care/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'care/index.html')
+def services(request):
+    return render(request,'care/services.html')
+def contact(request):
+    return render(request,'care/contact.html')
