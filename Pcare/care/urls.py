2f65f679abfd20ebd7cf5fb197de2392ba8d88c9
Chaithra K V <chaithrakv98@gmail.com>
2021-11-22 18:42:49 +0530

    login

--- Pcare/care/urls.py
+++ Pcare/care/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index,name="home"),
     path('services/',views.services, name="services"),
     path('contact/',views.contact, name="contact"),
     path('about/',views.about, name="about")
