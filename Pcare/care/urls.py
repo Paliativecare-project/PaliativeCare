f17129a6f51614dc779769b94b76a4ba813ad810
SRUTHI <sruthimolbiju21@gmail.com>
2021-11-26 16:18:50 +0530

    login

--- Pcare/care/urls.py
+++ Pcare/care/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('adminhome/',views.adminhome, name="adminhome"),
     path('adminservice/',views.adminservice,name="adminservice"),
     path('register/',views.register,name="register"),
-    path('login/',views.login,name="login"),
+    path('login/',views.login,name="servicelogin"),
     
 
     ]
