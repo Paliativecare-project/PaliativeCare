7662a850fdee938c20186c5dffef56a517308811
ananduv2 <ananduv2.0@gmail.com>
2021-12-10 00:59:18 +0530

    email changed

--- Pcare/Pcare/settings.py
+++ Pcare/Pcare/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anjuakt@gmail.com'
-EMAIL_HOST_PASSWORD = 'tuhmovufcvxblowi'
+EMAIL_HOST_USER = '1022@tkmce.ac.in'
+EMAIL_HOST_PASSWORD = 'exryqgwydxtqqhus'
