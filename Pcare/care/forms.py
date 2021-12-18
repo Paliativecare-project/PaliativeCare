8e6997d29a0ba669aaee0071f857973ac606f1c8
Chaithra K V <chaithrakv98@gmail.com>
2021-12-18 22:42:04 +0530

    commit

--- Pcare/care/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import fields
-from .models import Addservice,Servicesmodel
-
-class TodoForm(forms.ModelForm):
-    class Meta:
-        model=Addservice
-        fields=["id","s_name"]
